f146d01cd0c087508b5a5fa671f6f7995aa2f666
anoju <zun5761@gmail.com>
2023-03-20 16:34:26 +0900

    view-transition

---
+++ view-transition/waiting-for-fonts/with-waiting/script.js
@@ -0,0 +1,16 @@
+import {
+  getPageContent,
+  onLinkNavigate
+} from '../../utils.js';
+
+onLinkNavigate(async ({
+  toPath
+}) => {
+  const content = await getPageContent(toPath);
+  const transition = document.createDocumentTransition();
+
+  transition.start(async () => {
+    document.body.innerHTML = content;
+    await document.fonts.ready;
+  });
+});
