952d1a080cf6096b2336091bdaa1b04b115160f6
zun5761 <zun5761@gmail.com>
2024-11-11 17:07:21 +0900

    mockup.js re ver(2)

--- html-list/mockup.js
+++ html-list/mockup.js
@@ -34,18 +34,45 @@ const pubList = {
 	},
 	makeList(){
 		const pubPage = document.querySelector('.pub-page');
+		const pubHeader = document.querySelector('.pub-header');
 		const contentHtml = document.createElement('div');
     contentHtml.className = 'pub-content';
 		const groupDep1 = pubList.groupByDep1(pubJSON);
 		if(!groupDep1.length) return;
 		
+		const navAry = [];
 		const fragment = document.createDocumentFragment();
 		groupDep1.forEach(item => {
+			navAry.push(item.dep1);
 			const section = pubList.createSection(item);
 			fragment.appendChild(section);
 		});
 		contentHtml.appendChild(fragment);
 		pubPage.appendChild(contentHtml);
+
+		//pubList.createMobileFrame(pubPage);
+		pubList.createNav(pubHeader, navAry);
+
+		const countObj = {};
+		countObj.total = pubJSON.length; // 전체
+		countObj.unuse = pubJSON.filter(item => parseInt(item.count) === 0).length; // 미포함
+		countObj.useTotal = countObj.total - countObj.unuse;
+		countObj.del = pubJSON.filter(item => parseInt(item.STATUS) === 0 && parseInt(item.count) !== 0).length;
+		countObj.end = pubJSON.filter(item => item.END.trim() !== '' && parseInt(item.STATUS) !== 0 && parseInt(item.count) !== 0).length;
+		countObj.endDel = pubJSON.filter(item => item.END.trim() !== '' && parseInt(item.STATUS) === 0 && parseInt(item.count) !== 0).length;
+		countObj.wait = pubJSON.filter(item => parseInt(item.STATUS) === 1 && item.END.trim() === '' && parseInt(item.count) !== 0).length;
+		countObj.ing = pubJSON.filter(item => parseInt(item.STATUS) === 2 && item.END.trim() === '' && parseInt(item.count) !== 0).length;
+		countObj.chk = pubJSON.filter(item => parseInt(item.STATUS) === 3 && item.END.trim() === '' && parseInt(item.count) !== 0).length;
+		countObj.reChk = pubJSON.filter(item => parseInt(item.STATUS) === 4 && item.END.trim() === '' && parseInt(item.count) !== 0).length;
+		
+		pubList.createSide(pubHeader, countObj);
+		countObj.modify = pubJSON.filter(item => item.MODIFY.trim() !== '').length;
+		pubList.createSide(pubHeader, countObj);
+	},
+	getPercent (val1, val2){
+		const rtnVal = ((val1/val2)*100).toFixed(2);
+		if (isNaN(rtnVal)) rtnVal = 0;
+		return rtnVal;
 	},
 	groupByDep1 (data) {
 		// 먼저 객체로 그룹화
@@ -64,14 +91,180 @@ const pubList = {
 				items
 		}));
 	},
+	createMobileFrame(element){
+		const frameHtml = document.createElement('div');
+    frameHtml.className = 'pub-mobile-frame';
+		const frameInnerHtml = `
+			<div class="pub-mobile-frame-inr">
+					<div class="pub-device">
+							<select>
+									<option value="375*667">iPhone 6/7/8</option>
+									<option value="375*812">iPhone 12 Mini</option>
+									<option value="390*844">iPhone 12 Pro</option>
+									<option value="360*740" selected="selected">Galaxy S8+</option>
+									<option value="280*653">Galaxy Fold</option>
+							</select>
+					</div>
+					<div class="pub-screen">
+							<button type="button">&#8634;</button>
+							<div class="link"></div>
+					</div>
+					<div class="pub-iframe"><iframe src frameborder="0"></div>
+					<button type="button" class="pub-frame-toggle">
+							<span>닫기</span>
+					</button>
+			</div>
+		`;
+		frameHtml.innerHTML = frameInnerHtml;
+		element.appendChild(frameHtml);
+	},
+	createNav(element, data){
+		const navHtml = document.createElement('div');
+    navHtml.className = 'pub-nav';
+		let navListHtml ='';
+		if(data.length){
+			navListHtml += '<ul>';
+			data.forEach((item, i) => {
+				navListHtml += `<li><a href="#menu${i}" title="${item}"><span>${item}</span></a></li>`
+			});
+			navListHtml += '/<ul>';
+		}
+		const navInnerHtml = `
+			<div class="pub-nav-inr">
+				<div class="pub-all"><a href="#all" class="on"><span>전체</span></a></div>
+				<div class="pub-menu">${navListHtml}</div>
+			</div>
+		`;
+		navHtml.innerHTML = navInnerHtml;
+		element.appendChild(navHtml);
+	},
+	createSide(element, countObj, ){
+		const sideHtml = document.createElement('div');
+    sideHtml.className = 'pub-side';
+		const totalHtml = `<div class="pub-total">
+			<strong>
+				${countObj.end}
+				<span><em>${countObj.endDel}</em></span>
+			</strong>
+			<span>/</span>
+			<strong>
+				${countObj.useTotal - countObj.del}
+				<span><em>${countObj.del}</em></span>
+			</strong>
+			<em>
+				<span>
+					<span>진척률</span>
+					<strong>${pubList.getPercent(countObj.end, countObj.useTotal - countObj.del)}</strong>%
+				</span>
+				<button type="button" class="pub-button-detail" aria-expanded="false"></button>
+			</em>	
+			<ul class="pub-total-layer">
+					<li>
+							<em>전체<span>(<strong>${countObj.total}</strong>)</span></em>
+					</li>
+					<li class="unuse">
+							<em>미포함<span>(<strong>${countObj.unuse}</strong>)</span></em>
+							<span><strong>${pubList.getPercent(countObj.unuse, countObj.total)}</strong>%</span>
+					</li>
+					<li class="del">
+							<em>삭제<span>(<strong>${countObj.del}</strong>)</span></em>
+							<span><strong>${pubList.getPercent(countObj.del, countObj.total)}</strong>%</span>
+					</li>
+					<li class="wait">
+							<em>대기<span>(<strong>${countObj.wait}</strong>)</span></em>
+							<span><strong>${pubList.getPercent(countObj.wait, countObj.total)}</strong>%</span>
+					</li>
+					<li class="ing">
+							<em>퍼블중<span>(<strong>${countObj.ing}</strong>)</span></em>
+							<span><strong>${pubList.getPercent(countObj.ing, countObj.total)}</strong>%</span>
+					</li>
+					<li class="chk">
+							<em>재/검토중<span>(<strong>${countObj.chk+countObj.reChk}</strong>)</span></em>
+							<span><strong>${pubList.getPercent(countObj.chk+countObj.reChk, countObj.total)}</strong>%</span>
+					</li>
+					<li class="end">
+							<em>완료<span>(<strong>${countObj.end}</strong>)</span></em>
+							<span><strong>${pubList.getPercent(countObj.end, countObj.total)}</strong>%</span>
+					</li>
+			</ul>
+		</div>`;
+		/*
+		const util1Html = `<div class="pub-group">
+			<button class="pub-unlock on">&#8634;</button>
+			<div class="pub-record">
+				<button type="button" aria-expanded="false"># 전체</button>
+				<div class="pub-option">
+					<button type="button" data-value="optionAll" class="on"># 전체</button>
+					<div class="pub-option-inr">
+						<div class="progress"></div>
+						<div class="type"></div>
+						<div class="wbs"></div>
+						<div class="pub-team"></div>
+					</div>
+				</div>
+			</div>
+			<div class="pub-search">
+				<input type="text" placeholder="메뉴명"><button class="pub-search-btn"></button>
+			</div>
+			<button type="button" class="pub-viewer" aria-pressed="false"></button>
+			<button type="button" aria-pressed="false" class="pub-toggle"></button>
+		</div>`;
+		*/
+		const util1Html = '';
+		const alarmHtml = function(num){
+			if(num) return `<strong class="pub-alarm"><span>${num}</span></strong>`;
+			else return '';
+		}
+		const util2Html = `<div class="pub-label">
+			<ul>
+				<li class="wait">
+					${alarmHtml(countObj.wait)}
+					<button type="button" class="pub-filter-status"  ${countObj.wait?'':`disabled="disabled"`}><em>대기중</em></button>
+				</li>
+				<li class="ing">
+					${alarmHtml(countObj.ing)}
+					<button type="button" class="pub-filter-status"  ${countObj.ing?'':`disabled="disabled"`}><em>퍼블중</em></button>
+				</li>
+				<li class="chk">
+					${alarmHtml(countObj.chk+countObj.reChk)}
+					<button type="button" class="pub-filter-status"  ${countObj.chk+countObj.reChk?'':`disabled="disabled"`}><em>재/검토중</em></button>
+				</li>
+				<li class="del">
+					${alarmHtml(countObj.del)}
+					<button type="button" class="pub-filter-status" ${countObj.del?'':`disabled="disabled"`}><em>삭제</em></button>
+				</li>
+			</ul>
+			<ul>
+				<li class="history">
+					<button type="button" class="pub-modify-open" ${countObj.modify ? `disabled="disabled"`: ''}><span>수정이력</span></button>
+				</li>
+			</ul>
+			<ul>
+				<li class="down">
+					<button type="button" class="pub-csv-down"><span>CSV 다운</span></button>
+				</li>
+			</ul>
+		</div>`;
+		const sideInnerHtml = `
+			<div class="pub-side-inr">
+				${totalHtml}
+				<div class="pub-legend">
+					${util1Html}
+					${util2Html}
+				</div>
+			</div>
+		`;
+		sideHtml.innerHTML = sideInnerHtml;
+		element.appendChild(sideHtml);
+	},
 	createSection(data){
 		const dataTit = data.dep1.replace(/ /gi,"").replace(/[/]/gi, 'ㆍ').replace(/[(]/gi, '！').replace(/[)]/gi, '？');
 
 		const dataItems = data.items;
 		const fragment = document.createDocumentFragment();
-		dataItems.forEach(item => {
-			const row = pubList.createTableRow(item);
-			// fragment.appendChild(row);
+		dataItems.forEach((item, i) => {
+			const row = pubList.createTableRow(item, i);
+			fragment.appendChild(row);
 		});
 
 		const depKeys = ['DEP2', 'DEP3', 'DEP4', 'DEP5', 'DEP6'];
@@ -172,6 +365,24 @@ const pubList = {
 		`;
 		siteHtml.innerHTML = innerHtml;
 		siteHtml.querySelector('.pub-tbody tbody').append(fragment);
+		if(depList.length){
+			depList.forEach(item => {
+				if(item.counts.length === 0){
+					let removeEls = null;
+					if(item.key === 'DEP2') removeEls = siteHtml.querySelectorAll('.dep2');
+					else if(item.key === 'DEP3') removeEls = siteHtml.querySelectorAll('.dep3');
+					else if(item.key === 'DEP4') removeEls = siteHtml.querySelectorAll('.dep4');
+					else if(item.key === 'DEP5') removeEls = siteHtml.querySelectorAll('.dep5');
+					else if(item.key === 'DEP6') removeEls = siteHtml.querySelectorAll('.dep6');
+					if(removeEls){
+						removeEls.forEach(element => {
+							element.remove();
+						});
+					}
+				}
+			});
+		}
+		pubList.setColspan(siteHtml);
 		return siteHtml;
 	},
 	countByMultipleKeys (data, keys) {
@@ -190,7 +401,7 @@ const pubList = {
 				count,
 				items: data.filter(item => item[key] === value)
 			}));
-	}	;
+		}	;
 
     return keys.map(key => ({
         key,
@@ -208,8 +419,159 @@ const pubList = {
 		});
 		return rtnVal;
 	},
-	createTableRow(){
+	setColspan(element){
+		const theadTh = element.querySelectorAll('.pub-thead thead th');
+		const theadThVisible = Array.from(theadTh).filter(th => {
+			const style = window.getComputedStyle(th);
+			return style.display !== 'none';
+		});
+		const thLength = theadThVisible.length;
+		element.querySelectorAll('tr.sb').forEach(item => {
+			item.querySelector('td').setAttribute('colspan', thLength);
+		});
+	},
+	beforeTr :{
+		dep2: null,
+		dep3: null,
+		dep4: null,
+		dep5: null,
+		dep6: null,
+	},
+	createTableRow(rowData, index){
+		const count = parseInt(rowData.COUNT.trim());
+		// const depth1Name = rowData.DEP1.replace(/ /gi,"").replace(/[/]/gi, 'ㆍ').replace(/[(]/gi, '！').replace(/[)]/gi, '？');
+		const depth2Name = rowData.DEP2.trim();
+		const depth3Name = rowData.DEP3.trim();
+		const depth4Name = rowData.DEP4.trim();
+		const depth5Name = rowData.DEP5.trim();
+		const depth6Name = rowData.DEP6.trim();
+		const screen = rowData.SCREEN.trim();
+		const type = rowData.TYPE.trim();
+		const size = rowData.SIZE ? rowData.SIZE.trim() : null;
+		const url = rowData.URL.trim();
+		const id = rowData.ID.trim();
+		const publisher = rowData.PUB.trim();
+		const designer = rowData.DGN.trim();
+		const planner = rowData.PLA.trim();
+		const developer = rowData.DEV.trim();
+		const status = parseInt(rowData.STATUS.trim());
+		const schedule = rowData.WBS.trim();
+		const end = rowData.END.trim();
+		const modify = rowData.MODIFY.trim();
+		const memo = rowData.MEMO.trim();
+		const sb = rowData.SB.trim();
 
+		const fragment = document.createDocumentFragment();
+		if(sb) {
+			const sbHtml = document.createElement('tr');
+			sbHtml.className = 'sb';
+			sbHtml.innerHTML = `<td><em>${sb}</em></td>`;
+			fragment.appendChild(sbHtml);
+		}
+
+		const trClassAry = ['tr'];
+		if(depth2Name) trClassAry.push('tr-dep2_'+depth2Name);
+		if(depth3Name) trClassAry.push('tr-dep3_'+depth3Name);
+		if(depth4Name) trClassAry.push('tr-dep4_'+depth4Name);
+		if(depth5Name) trClassAry.push('tr-dep5_'+depth5Name);
+		if(depth6Name) trClassAry.push('tr-dep6_'+depth6Name);
+		if(id) trClassAry.push('tr-id_'+id);
+		if(count === 0) {
+			trClassAry.push('unuse');
+		}else{
+			if(end){
+				if(status === 0) trClassAry.push('del');
+				else trClassAry.push('end');
+				const endStr = end.replace(/-/g, '');
+				trClassAry.push('tr-end_'+endStr);
+			}else{
+				if(status === 0) trClassAry.push('del');
+				else if(status === 1) trClassAry.push('wait');
+				else if(status === 2) trClassAry.push('ing');
+				else if(status === 3) trClassAry.push('chk');
+				else if(status === 4) trClassAry.push('chk re');
+			}
+		}
+
+		const depth2Html = pubList.beforeTr.dep2 === depth2Name ? `<span>${depth2Name}</span>` : `<strong>${depth2Name}</strong>`;
+		const depth3Html = pubList.beforeTr.dep3 === depth3Name ? `<span>${depth3Name}</span>` : `<strong>${depth3Name}</strong>`;
+		const depth4Html = pubList.beforeTr.dep4 === depth4Name ? `<span>${depth4Name}</span>` : `<strong>${depth4Name}</strong>`;
+		const depth5Html = pubList.beforeTr.dep5 === depth5Name ? `<span>${depth5Name}</span>` : `<strong>${depth5Name}</strong>`;
+		const depth6Html = pubList.beforeTr.dep6 === depth6Name ? `<span>${depth6Name}</span>` : `<strong>${depth6Name}</strong>`;
+
+		const typeTd = size ? `<td class="type" data-size="${size}">${type}</td>`: `<td class="type">${type}</td>`;
+
+		const idTd = () => {
+			let rtnVal = id;
+			const preUrl = '..';
+			const file = typeof pubSetting !== 'undefined' && typeof pubSetting.file !== 'undefined' ? pubSetting.file : 'html';
+			if(url && id && status !== 1){
+				const setUrl = url.slice(-1) === '/' ? preUrl+url+id+'.'+file : preUrl+url;
+				rtnVal = `<a href="${setUrl}" target="_blank"><strong>${id}</strong></a><button type="button" class="pub-copy" title="메뉴복사"></button>`;
+			}
+			return rtnVal;
+		}
+
+		const weekdays = ['일', '월', '화', '수', '목', '금', '토'];
+		const getWeek = (date) => {
+			let rtnVal = weekdays[new Date(date).getDay()]
+			if(rtnVal) return `(${rtnVal})`;
+			else return '';
+		}
+
+		const convertModifyList = (htmlString) => {
+			// 각각의 li 항목을 매칭하는 정규식
+			const regex = /\[(\d{4}-\d{2}-\d{2}) (.*?)\]/g;
+			// replace 함수를 사용하여 매칭되는 모든 항목을 변환
+			return htmlString.replace(regex, (match, dateStr, content) => {
+					const weekday = getWeek(dateStr);
+					
+					const dateStr2 = dateStr.replace(/-/g, '');
+					trClassAry.push('tr-modify_'+dateStr2);
+
+					// 새로운 형식으로 변환
+					return `<li title="${dateStr} ${content}"><em><span>${dateStr}</span>${weekday}</em><p>${content}</p></li>`;
+			});
+		};
+		const getModifyTd = () => {
+			const txt = modify.replace(/\[/g, '').replace(/\]/g, '').trim();
+			const liHtml = convertModifyList(modify);
+			return txt ? `<button type="button" aria-pressed="false"></button><ul>${liHtml}</ul>`: '';
+		};
+		const modifyTd = getModifyTd();
+		
+		const trHtml = document.createElement('tr');
+		trHtml.className = trClassAry.join(' ');
+		const trInnerHtml = `
+			<td class="no"><span>${index+1}</span></td>
+			<td class="dep2">${depth2Html}</td>
+			<td class="dep3">${depth3Html}</td>
+			<td class="dep4">${depth4Html}</td>
+			<td class="dep5">${depth5Html}</td>
+			<td class="dep6">${depth6Html}</td>
+			<td class="screen"><span>${screen}</span></td>
+			${typeTd}
+			<td class="url">${url}</td>
+			<td class="id">${idTd()}</td>
+			<td class="publisher">${publisher}</td>
+			<td class="designer">${designer}</td>
+			<td class="planner">${planner}</td>
+			<td class="developer">${developer}</td>
+			<td class="wbs">${schedule}${getWeek(schedule)}</td>
+			<td class="status">${status}</td>
+			<td class="modify">${modifyTd}</td>
+			<td class="memo">${memo}</td>
+		`;
+		trHtml.innerHTML = trInnerHtml;
+		fragment.appendChild(trHtml);
+
+		if(depth2Name) pubList.beforeTr.dep2 = depth2Name;
+		if(depth3Name) pubList.beforeTr.dep3 = depth3Name;
+		if(depth4Name) pubList.beforeTr.dep4 = depth4Name;
+		if(depth5Name) pubList.beforeTr.dep5 = depth5Name;
+		if(depth6Name) pubList.beforeTr.dep5 = depth6Name;
+
+		return fragment;
 	}
 }
 
